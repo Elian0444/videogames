b85191d9dffac41b6ba7e89a0cb0712d7568717a
Elian <nanoncio3@gmail.com>
2024-09-13 21:35:59 -0500

    game_race

---
+++ script_python/Number_race/main.py
@@ -0,0 +1,11 @@
+'''script description: number race
+    dev: Elian
+    Date: 13-09-2024
+'''
+from random import randint
+
+dice1  = randint(1,6)
+dice2  = randint(1,6)
+
+print(f"dice1: {dice1}")
+print(f"dice2: {dice2}")
